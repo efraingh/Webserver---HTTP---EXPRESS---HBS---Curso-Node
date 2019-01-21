d6dd5011c2fcf3be9f51f9a9ab9d2d5b8b3243ae
Inefra <inefra@KalDrago>
2019-01-20 20:57:42 -0400

    Primer Comit

--- app-old.js
+++ app-old.js
@@ -13,6 +13,6 @@ http.createServer( (req, res ) => {
     //res.write('Hola Mundo');
     res.end();
 })
-.listen(8080);
+.listen(8081);
 
-console.log('Escuchando el puerto 8080');
+console.log('Escuchando el puerto 8081');
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+//middelware
+
+app.use(express.static(__dirname + '/public'))
+
+// Express HBS
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    //res.send('Hola Mundo');
+    // let salida = {
+    //     nombre: 'efrain llusco',
+    //     edad: 30,
+    //     url: req.url
+    // }
+    //res.send(salida);
+    res.render('home',{
+        nombre: 'efris',
+        anio: new Date().getFullYear()
+    });
+});
+
+/*app.get('/data', (req, res) => {
+    res.send('Hola Data');
+});*/
+
+app.listen(3000, ()=>{
+    console.log('Escuchando peticiones en el puerto 3000');
+});
